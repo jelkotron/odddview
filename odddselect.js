9efd9ed8b5a9b44095afd91859733cead7a6be9a
schnollie <ojelko@proton.me>
2024-11-25 23:41:17 +0100

    implements hide state for back to root button when at root

--- odddselect.js
+++ odddselect.js
@@ -2,60 +2,66 @@
 import { OdddViewer } from 'viewer';
 import { OdddData } from 'viewer';
 
-
 class GuiElements{
     constructor(container, dictionary=undefined, root_name=undefined){
         this.dictionary = dictionary;
         this.highlight = false;
-        let div = document.createElement("div");
-        div.className = "div_3d_gui";
-        
-        let infoKeyArrows = document.createElement("p");
-        infoKeyArrows.className = "infokey3darrows";
-        infoKeyArrows.innerHTML = "<<";
-        div.appendChild(infoKeyArrows)
+        this.hidden = false;
+        this.div = document.createElement("div");
+        this.div.className = "div_3d_gui";
+        this.root_name = root_name;
+        this.infoKeyArrows = document.createElement("p");
+        this.infoKeyArrows.className = "infokey3darrows";
+        this.infoKeyArrows.innerHTML = "<<";
+        this.div.appendChild(this.infoKeyArrows)
 
-        let infoKeyArrowsHelp = document.createElement("p");
-        infoKeyArrowsHelp.className = "infokey3darrowshelp";
-        infoKeyArrowsHelp.innerHTML = "back to " + root_name;
-        div.appendChild(infoKeyArrowsHelp)
+        this.infoKeyArrowsHelp = document.createElement("p");
+        this.infoKeyArrowsHelp.className = "infokey3darrowshelp";
+        this.infoKeyArrowsHelp.innerHTML = "back to " + this.root_name;
+        this.div.appendChild(this.infoKeyArrowsHelp)
         
-        let infoKey = document.createElement("p");
-        infoKey.className = "infokey3d";
-        infoKey.innerHTML = "";
-        div.appendChild(infoKey)
+        this.infoKey = document.createElement("p");
+        this.infoKey.className = "infokey3d";
+        this.infoKey.innerHTML = "";
+        this.div.appendChild(this.infoKey)
         
-        container.append(div);    
-        
-        this.infoKeyArrows = infoKeyArrows;
-        this.infoKeyArrowsHelp = infoKeyArrowsHelp;
-        this.infoKey = infoKey;
-        this.div = div;
+        container.append(this.div);    
+        this.hide();
     }
 
     pointToElement(elementname){
         if(this.dictionary && this.dictionary[elementname]){
             elementname = this.dictionary[elementname];
         }
-        this.infoKeyArrows.innerHTML = ">>  ";
+        this.infoKeyArrows.innerHTML = ">>";
         this.infoKeyArrows.style.fontWeight = 'bolder';
         this.infoKey.innerHTML = elementname;
     }
     pointToRoot = function(){
         this.infoKey.innerHTML = ""
-        this.infoKeyArrows.innerHTML = "<<";
+        if(self.hidden === false){
+            this.infoKeyArrows.innerHTML = "<<";
+        }
+        else{
+            this.infoKeyArrows.innerHTML = "";
+        }
     
         if(this.highlight === true){
             this.infoKeyArrows.style.fontWeight = 'bolder';
-            
         }
         else{
             this.infoKeyArrows.style.fontWeight = 'lighter';
         }
     }
     hide = function(){
-        this.infoKey.innerHTML = ""
+        self.hidden = true;
+        this.infoKey.innerHTML = "";
         this.infoKeyArrows.innerHTML = "";
+        this.infoKeyArrowsHelp.innerHTML = "";
+    }
+    unhide = function(){
+        self.hidden = false;
+        this.infoKeyArrowsHelp.innerHTML = "back to " + this.root_name;
     }
 }
 
@@ -64,20 +70,24 @@ class GuiElements{
 function main(){    
     // get json url from data attributes (variables passed to script tag)
     const script = document.scripts[document.scripts.length - 1];
-    
     const data_url = script.dataset.jsondata;
+    
     // get container
     const container = script.parentNode;
     
+    // get data, add objects 
     const response = fetch(data_url).then(res => res.json()).then(rawdata => {
-        let intersects = [];
         const jsondata = JSON.parse(rawdata);
         const data = new OdddData(jsondata);
         const dictionary = data["dictionary"];
-        const gui = new GuiElements(container, data["dictionary"], data["root_name"]);
+        
+        const gui = new GuiElements(container, dictionary, data["root_name"]);
         const viewer = new OdddViewer(container, data);
+        
         let block_container = document.getElementsByClassName("block_container")[0];
-
+        
+        let intersects = [];
+        
         gui.div.addEventListener("mouseenter", function(){
             gui.highlight = true;
         });
@@ -90,6 +100,8 @@ function main(){
             viewer.setPrimaryHighlight(null);     
             const response = fetch('blocks/').then(res => res.text()).then(txt => {
                 block_container.innerHTML = txt;
+            }).then(undefined => {
+                gui.hide()
             })
         })   
 
@@ -102,9 +114,10 @@ function main(){
                 block_container.innerHTML = "";
                 let key = intersects[0].object.name;
                 let value = dictionary[key];
-    
                 const response = fetch('blocks/' + key).then(res => res.text()).then(htmltext => {
                     block_container.innerHTML = htmltext;
+                }).then(undefined => {
+                    gui.unhide();
                 })
             }
         });
